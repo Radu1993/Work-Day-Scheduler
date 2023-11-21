f2d23bf322819d84582b41f50a3b1530bf770d20
DESKTOP-UDFQ2B6\Dell <oros.radu93@yahoo.com>
2023-11-21 20:09:28 +0000

    fresh commit

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,49 @@
 $(document).ready(function () {
-    console.log("Ready");
-});
+    //Moment.js code for current date and time
+    let NowMoment = moment().format("MMMM Do YYYY");
+    let displayDate = document.getElementById("currentDay");
+    displayDate.innerHTML = NowMoment;
+    let currentHour = moment().format("HH");
 
-// Display the current day
-let now = moment().format("dddd, MMMM Do YYYY");
-let displayDate = document.getElementById("currentDay");
-displayDate.innerHTML = now;
-let currentHour = moment().format("HH");
+    // Button function to clear local storage and clear contents
+    $("#clearFieldsBtn").click(function (event) {
+        event.preventDefault;
+        $("textarea").val("");
+        localStorage.clear();
+    });
 
-//Past , present and futurre timeblock
-$(".time-div").each(function () {
-    var timeDiv = $(this).attr("id").split("-")[1];
+    //grabs hour from each time slot and compares it to actual time
+    $(".time-div").each(function () {
+        var timeDiv = $(this).attr("id").split("-")[1];
 
-    if (currentHour == timeDiv) {
-        $(this).addClass("present");
-        $(thi).children(".description").addClass("present");
-    } else if (currentHour < timeDiv) {
-        $(this).removeClass("present");
-        $(this).addClass("future");
-    } else if (currentHour > timeDiv) {
-        $(this).removeClass("future");
-        $(this).addClass("past");
-    }
-});
+        if (currentHour == timeDiv) {
+            $(this).addClass("present");
+            $(this).children(".description").addClass("white-text");
+        } else if (currentHour < timeDiv) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        } else if (currentHour > timeDiv) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+    });
 
-//Save the event in local storage
-$(".saveBtn").click(function (event) {
-    event.preventDefault();
-    var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id").split("-")[1];
-    localStorage.setItem(time, value);
-});
+    //grabs values from time and value divs and saves them to local storage
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        var value = $(this).siblings(".time-block").val();
+        var time = $(this).parent().attr("id").split("-")[1];
+        localStorage.setItem(time, value);
+    });
 
-//timeblocks for standard business hours
-$("#hour-09 .time-block").val(localStorage.getItem("hour-09"));
-$("#hour-10 .time-block").val(localStorage.getItem("hour-10"));
-$("#hour-11 .time-block").val(localStorage.getItem("hour-11"));
-$("#hour-12 .time-block").val(localStorage.getItem("hour-12"));
-$("#hour-13 .time-block").val(localStorage.getItem("hour-13"));
-$("#hour-14 .time-block").val(localStorage.getItem("hour-14"));
-$("#hour15 .time-block").val(localStorage.getItem("hour-15"));
-$("#hour16 .time-block").val(localStorage.getItem("hour-16"));
-$("#hour17 .time-block").val(localStorage.getItem("hour-17"));
-
-$("#clearFieldsBtn").click(function (event) {
-    event.preventDefault;
-    $("textArea").val("");
-    localStorage.clear();
+    //retrieves items from local storage and sets them in proper places
+    $("#hour-09 .time-block").val(localStorage.getItem("09"));
+    $("#hour-10 .time-block").val(localStorage.getItem("10"));
+    $("#hour-11 .time-block").val(localStorage.getItem("11"));
+    $("#hour-12 .time-block").val(localStorage.getItem("12"));
+    $("#hour-13 .time-block").val(localStorage.getItem("13"));
+    $("#hour-14 .time-block").val(localStorage.getItem("14"));
+    $("#hour-15 .time-block").val(localStorage.getItem("15"));
+    $("#hour-16 .time-block").val(localStorage.getItem("16"));
+    $("#hour-17 .time-block").val(localStorage.getItem("17"));
 });
